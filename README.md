e253100cd835a9e502ce2a71b119fe008211687d
Leonard Woo <leo_916@yahoo.com>
2022-09-26 10:03:59 +0800

    0.1.1 fixed doc, update class

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # Pika JS
 ![GitHub](https://img.shields.io/github/license/leonardwoo/pika-js?style=flat-square)
+![npm (tag)](https://img.shields.io/npm/v/@leonardwoo/pika-js/latest?style=flat-square)
+
+## Introduction
 
 A function toolkit
+
+## CDN
+
+[jsDelivr](https://www.jsdelivr.com/package/npm/@leonardwoo/pika-js)
+
+## Browser Use
+
+`<script src="https://cdn.jsdelivr.net/npm/@leonardwoo/pika-js@latest/dist/pika.min.js" defer></script>`
 
 ## Document
 
@@ -17,4 +28,14 @@ window.addEventListener('load', (event) => {
 
 ### `InvaildInfo(inputId='',inputEId='', regex='', message='')`
 
-Add or remove a `p` tag below the tag to display validation information.
+Add or remove a `p` tag with `inputEId` below the tag to display validation information.
+
+like this:
+```html
+<div>
+    <input type="text" id="{{inputId}}" />
+    <p id="{{inputEId}}">{{message}}</P>
+</div>
+```
+
+<!-- ## Sponsors -->
--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
 // Leonard Woo
 "use strict";
 
-function getEleHeight(ele) {
-  var e = document.querySelector(ele);
-  if (e == null) {
-    return 0;
-  }
-  var h = e.clientHeight;
-  if (h == null || h < 0) {
-    h = 0;
-  }
-  return h;
-}
+class Pikajs {
 
-function isSelection() {
-  var e = document.getElementsByTagName('section')[0]
-  if (e == null) {
-    return false;
+  constructor() {
   }
-  var h = e.clientHeight;
-  if (h == null || h < 0) {
-    return false;
+
+  static getEleHeight(ele) {
+    var e = document.querySelector(ele);
+    if (e == null) {
+      return 0;
+    }
+    var h = e.clientHeight;
+    if (h == null || h < 0) {
+      h = 0;
+    }
+    return h;
   }
-  return true;
+  
+  static isSelection() {
+    var e = document.getElementsByTagName('section')[0]
+    if (e == null) {
+      return false;
+    }
+    var h = e.clientHeight;
+    if (h == null || h < 0) {
+      return false;
+    }
+    return true;
+  }
 }
 
 function CalcMinMain() {
-  let headerHeight = getEleHeight('header');
-  let selectionHeight = isSelection();
-  let footerHeight = selectionHeight? 0: getEleHeight('footer');
+  let headerHeight = Pikajs.getEleHeight('header');
+  let selectionHeight = Pikajs.isSelection();
+  let footerHeight = selectionHeight? 0: Pikajs.getEleHeight('footer');
   let main = document.querySelector('main');
   main.style.setProperty('min-height', 'calc(100vh - ' + (headerHeight + footerHeight) + 'px)');
 }
